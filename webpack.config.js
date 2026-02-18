933567aefc5433d9fabd8edb10d4237493d2051c
Mustafa Savul <mustafasavul4434@hotmail.com>
2026-02-18 16:04:33 +0300

    chore: initial project setup

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: "development",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", "tsx.", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ],
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    open: true
+  }
+}
