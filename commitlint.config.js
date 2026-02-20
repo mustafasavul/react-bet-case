04e609746e79c2aee21c47baf0c7028804060ee7
Mustafa Savul <mustafasavul4434@hotmail.com>
2026-02-20 04:33:08 +0300

    refactor: refine component styling.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [0, 'always'],
+    'subject-full-stop': [0, 'never'],
+    'type-case': [0, 'always'],
+    'header-max-length': [1, 'always', 120],
+  },
 };
